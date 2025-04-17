5ac92c62d347d73dc8409d96e0f79f381f6b9643
Mayank Kumar <rajmayank3024@gmail.com>
2025-04-17 13:03:53 +0530

    Restore the previous version of Project

--- src/main/java/com/assignment/dto/UserUpdateDto.java
+++ src/main/java/com/assignment/dto/UserUpdateDto.java
@@ -2,12 +2,9 @@ package com.assignment.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-
 public class UserUpdateDto {
 
     @NotBlank(message = "Name is required!")
--- src/main/java/com/assignment/security/JwtUtil.java
+++ src/main/java/com/assignment/security/JwtUtil.java
@@ -4,10 +4,8 @@ package com.assignment.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
 import java.util.Date;
 import java.util.function.Function;
 
